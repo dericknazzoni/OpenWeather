df89dc7daa3ed89d2b485e4979da882de482537e
Derick Nazzoni <dericknazzoni00@gmail.com>
2024-08-09 21:44:39 -0300

    First commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by derick nazzoni on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
